f4b3dae65e225e9cdc8f02045c1c82caf70a02d9
Rasmus von Walden <rvwalden@cisco.com>
2024-05-21 17:02:41 +0200

    Updated logic

--- nac_collector/cisco_client_ndo.py
+++ nac_collector/cisco_client_ndo.py
@@ -7,6 +7,21 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 logger = logging.getLogger("main")
 
+def merge_dict_list(source, destination):
+    for key, value in source.items():
+        if isinstance(value, dict):
+            # get node or create one
+            node = destination.setdefault(key, {})
+            merge_dict_list(value, node)
+        elif isinstance(value, list):
+            if key not in destination:
+                destination[key] = []
+            if isinstance(destination[key], list):
+                destination[key].extend(value)
+        else:
+            destination[key] = value
+    return destination
+
 
 class CiscoClientNDO(CiscoClient):
 
@@ -59,16 +74,52 @@ class CiscoClientNDO(CiscoClient):
 
         for endpoint in endpoints:
             if all(x not in endpoint.get("endpoint",{}) for x in ["%v", "%i"]):
+
                 endpoint_dict = CiscoClient.create_endpoint_dict(endpoint)
                 response = self.get_request(self.base_url + endpoint["endpoint"])
 
                 data = response.json()
 
+
                 if isinstance(data, list):
-                    endpoint_dict[endpoint["name"]]["items"] = data
+                    endpoint_dict[endpoint["name"]] = data
                 elif isinstance(data, dict):
-                    endpoint_dict[endpoint["name"]]["items"] = data[endpoint["name"]]
+                    endpoint_dict[endpoint["name"]] = data[endpoint["name"]]
+
+                if endpoint["name"] in final_dict and endpoint["key"]:
+                    for i,v in enumerate(endpoint_dict[endpoint["name"]]):
+                        for i1,v1 in enumerate(final_dict[endpoint["name"]]):
+                            if v.get(endpoint["key"]) == v1.get(endpoint["key"]):
+                                endpoint_dict[endpoint["name"]][i] = merge_dict_list(v1,v)
+                                break
 
                 final_dict.update(endpoint_dict)
+
+            else:
+                parent_endpoint = ""
+                parent_path = "/".join(endpoint.get("endpoint").split("/")[:-1])
+                for e in endpoints:
+                    if parent_path in e.get("endpoint") and e != endpoint:
+                        parent_endpoint = e
+                        break
+                if parent_endpoint and parent_endpoint.get("name") in final_dict:
+                    endpoint_dict = CiscoClient.create_endpoint_dict(endpoint)
+                    
+                    r = []
+
+                    for tmpl in final_dict[parent_endpoint["name"]]:
+                        reponse = self.get_request(self.base_url + endpoint["endpoint"].replace("%v",tmpl.get("templateId")))
+                        
+                        data = reponse.json()
+                        r.append(data)
+
+                    print(endpoint_dict)
+
+                    final_dict.update(
+                        {
+                            endpoint["name"]: r
+                        }
+                    )
+
         
         return final_dict
