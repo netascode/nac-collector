c9d46c20c3c42e7c491d9fc29615034349fea6a8
Rasmus von Walden <rvwalden@cisco.com>
2024-05-22 06:27:07 +0200

    Moved merge to utils.py

--- nac_collector/cisco_client_ndo.py
+++ nac_collector/cisco_client_ndo.py
@@ -2,26 +2,11 @@ from nac_collector.cisco_client import CiscoClient
 import requests
 import logging
 import urllib3
+from nac_collector.utils import merge_dict_list
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 logger = logging.getLogger("main")
-
-def merge_dict_list(source, destination):
-    for key, value in source.items():
-        if isinstance(value, dict):
-            # get node or create one
-            node = destination.setdefault(key, {})
-            merge_dict_list(value, node)
-        elif isinstance(value, list):
-            if key not in destination:
-                destination[key] = []
-            if isinstance(destination[key], list):
-                destination[key].extend(value)
-        else:
-            destination[key] = value
-    return destination
-
 
 class CiscoClientNDO(CiscoClient):
 
